70c145bfc6b5059ec91dc4fb42652363c1764dcc
CrazyBoy49z <yura.finiv@gmail.com>
2024-07-07 00:41:00 +0300

    Add default session key and cart session expiration to config
    A default session key for cart sessions was added into the cart configuration, along with a specification for the number of days to keep the cart session. This will ensure better control and management over the user's cart sessions.

--- config/lazy/cart.php
+++ config/lazy/cart.php
@@ -4,4 +4,8 @@
 return [
     'user_model' => 'App\Models\User',
     'table_prefix' => 'lazy_', // table prefix for cart table
+    'default_session_key' => 'cart_session_id', // session key for cart session
+    'cart' => [
+        'days' => 30, // days to keep cart session
+    ],
 ];
