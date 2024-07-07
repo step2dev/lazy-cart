6a0c135578f4c89122be3db2c602b1f442e7173e
CrazyBoy49z <yura.finiv@gmail.com>
2024-07-07 21:51:24 +0300

    Update cart configuration settings
    Changed the `default_session_key` to `name` under 'cart' from root level in the cart configuration file. This modification ensures the configuration setup aligns with the updated coding convention and improves code readability.

--- config/lazy/cart.php
+++ config/lazy/cart.php
@@ -4,13 +4,13 @@
 return [
     'user_model'          => 'App\Models\User',
     'table_prefix'        => 'lazy_', // table prefix for cart table
-    'default_session_key' => 'cart_session_id', // session key for cart session
     'cart'                => [
-        'days' => 30, // days to keep cart session
+        'days' => 30, // days to keep cart
+        'name' => 'cart_session', // name of the cart session
     ],
-    'money_format' => [
+    'money_format'        => [
         'separator' => '.',
-        'thousand' => ',',
+        'thousand'  => ',',
         'precision' => 2,
     ],
 ];
